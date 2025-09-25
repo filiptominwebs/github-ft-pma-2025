88902f698eb10a4f274b3ca1689f4a795ec4a7ea
Filip Tomín <77965577+filiptominwebs@users.noreply.github.com>
2025-09-25 12:09:53 +0200

    initial commit

---
+++ Cviko001/app/src/main/java/com/example/cviko001/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.cviko001
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+
+        val etFirstName = findViewById<EditText>(R.id.etFirstName)
+        val etLastName = findViewById<EditText>(R.id.etLastName)
+        val etCity = findViewById<EditText>(R.id.etCity)
+        val etAge = findViewById<EditText>(R.id.etAge)
+        val tvInformation = findViewById<TextView>(R.id.tvInformation)
+        val btnSend = findViewById<Button>(R.id.btSend)
+        val btnDelete = findViewById<Button>(R.id.btdelete)
+    }
+}
