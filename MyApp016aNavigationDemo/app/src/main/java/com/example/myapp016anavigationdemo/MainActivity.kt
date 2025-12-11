9972e54e1403a4573a7e5466ebbdba9a07ac0dce
Filip Tomín <77965577+filiptominwebs@users.noreply.github.com>
2025-12-11 11:46:08 +0100

    navigation app

---
+++ MyApp016aNavigationDemo/app/src/main/java/com/example/myapp016anavigationdemo/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapp016anavigationdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val navHost = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHost.navController
+
+        // Najdeme toolbar
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+
+        // Nastavíme ho jako ActionBar
+        setSupportActionBar(toolbar)
+
+        // Až teď může NavigationUI pracovat s ActionBarem
+        setupActionBarWithNavController(navController)
+
+        findViewById<BottomNavigationView>(R.id.bottomNav)
+            .setupWithNavController(navController)
+
+        findViewById<NavigationView>(R.id.drawerNav)
+            .setupWithNavController(navController)
+    }
+}
